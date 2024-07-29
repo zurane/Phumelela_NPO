4315293732d7444191a12cdf0b464d7d36a03714
Mpho Lebona <63550466+zurane@users.noreply.github.com>
2024-07-29 17:34:28 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,42 @@
+// Loader script
+
+document.addEventListener("DOMContentLoaded", function () {
+  const overlay = document.getElementById("overlay");
+  const content = document.getElementById("content");
+  // only show our loader when the document loads content
+  window.addEventListener("load", () => {
+    document.body.classList.add("loaded");
+  });
+});
+
+
+// Get the modal, open button, and close button elements
+const modalOverlay = document.getElementById('modalOverlay');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModalBtn = document.getElementById('closeModalBtn');
+const modalPopUp = document.querySelector('.modals')
+
+// Function to open the modal
+function openModal() {
+  modalOverlay.classList.add('active');
+  modalPopUp.classList.remove('in-active')
+}
+
+// Function to close the modal
+function closeModal() {
+  modalOverlay.classList.remove('active');
+  modalPopUp.classList.add('in-active')
+}
+
+// Add event listeners to open and close buttons
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+
+// Add event listener to close the modal when clicking outside of it
+window.addEventListener('click', (event) => {
+  if (event.target === modalOverlay) {
+    closeModal();
+  }
+});
+
 
